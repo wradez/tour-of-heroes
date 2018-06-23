ea66251b1ba0fb1bf40458f49fa1fc79de5d384a
Brook Trout <walkerradez@gmail.com>
2018-06-23 15:27:56 -0600

    Got the hero editor working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Component({ //a decorator
+  selector: 'app-root', //where to render on teh page
+  templateUrl: './app.component.html', //teh html document that will be associated with this component
+  styleUrls: ['./app.component.css'] // teh styles associated with this component
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
 }
